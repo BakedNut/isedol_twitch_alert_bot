cc19ffbae0fe9f8208eff0d77280078128060737
BakedNut <joonkee1219@gmail.com>
2022-12-15 15:23:59 +0900

    Change : Optimization

--- chat_alert.py
+++ chat_alert.py
@@ -1,6 +1,5 @@
 import connect
-import socket
-import time
+import asyncio
 from json import loads
 
 print('ISEDOL CHAT ALERT START')
@@ -10,23 +9,22 @@ TG = connect.ConnectTG("private") # Input "private" or "public"
 bot = TG.bot
 
 # Channels
-tracking_id = ("vo_ine", "jingburger", "lilpaaaaaa", "cotton__123", "gosegugosegu", "viichan6", "woowakgood", "chunyangkr", "111roentgenium")
+track = ("vo_ine", "jingburger", "lilpaaaaaa", "cotton__123", "gosegugosegu", "viichan6", "woowakgood", "chunyangkr", "111roentgenium")
 isedol_id = ("vo_ine", "jingburger", "lilpaaaaaa", "cotton__123", "gosegugosegu", "viichan6", "woowakgood")
 isedol_kr = {"vo_ine":"아이네 ⚪️", "jingburger":"징버거 🟡", "lilpaaaaaa":"릴파 🔵", "cotton__123":"주르르 🟣"
              , "gosegugosegu":"고세구 🦠", "viichan6":"비챤 🟢", "woowakgood":"우왁굳 🐵", "chunyangkr":"천양 🐡", "111roentgenium":"뢴트게늄 ☢"}
 
-selID = tracking_id[0] #Ine
-
-def run(ID):
+async def run(ID):
     SERVER = "irc.twitch.tv"
     PORT = 6667
+    IRCread: asyncio.StreamReader
+    IRCwrite: asyncio.StreamWriter
     PASSWORD = connect.Autho.OAuth  # This needs to be an OAuth token
     USERNAME = ID # Connect This Channel ID
     CHANNEL = USERNAME
     Connecting = True
-    IRC = socket.socket()
-    IRC.connect((SERVER, PORT))
-    IRC.send(
+    IRCread, IRCwrite = await asyncio.open_connection(SERVER, PORT)
+    IRCwrite.write(
         (
             "PASS " + PASSWORD + "\n" +
             "NICK " + USERNAME + "\n" +
@@ -34,9 +32,11 @@ def run(ID):
         )
         .encode()
     )
+    await IRCwrite.drain()
+    
     while Connecting:
         try:
-            readbuffer_join = IRC.recv(1024)
+            readbuffer_join = await IRCread.read(1024)
             readbuffer_join = readbuffer_join.decode()
             for line in readbuffer_join.split("\n")[0:-1]:
                 if ("End of /NAMES list" in line):
@@ -44,9 +44,8 @@ def run(ID):
                     Connecting = False
         except:
             print(f'{ID} Connect Fail.. Retry')
-            IRC = socket.socket()
-            IRC.connect((SERVER, PORT))
-            IRC.send(
+            IRCread, IRCwrite = await asyncio.open_connection(SERVER, PORT)
+            IRCwrite.write(
                 (
                     "PASS " + PASSWORD + "\n" +
                     "NICK " + USERNAME + "\n" +
@@ -54,12 +53,11 @@ def run(ID):
                 )
                 .encode()
             )
+            await IRCwrite.drain()
                 
     while True:
-        try:
-            readbuffer = IRC.recv(1024).decode()
-        except:
-            readbuffer = ""
+        readbuffer = await IRCread.read(1024)
+        readbuffer = readbuffer.decode()
         for line in readbuffer.split("\r\n"):
             if "PRIVMSG" in line:
                 separate = line.split(":", 2)
@@ -74,10 +72,18 @@ def run(ID):
                     else:
                         bot.sendMessage(chat_id=TG.chat_id, text= isedol_kr[user] + " → " + isedol_kr[USERNAME] + ":\n" + message)
             elif "PING" in line:
-                print("Received a PING")
+                print(f"Received a PING : {ID}")
                 message = "PONG tmi.twitch.tv\r\n".encode()
-                IRC.send(message)
+                IRCwrite.write(message)
+                await IRCwrite.drain()
                 print("Sent a PONG")
 
+# Define execute function
+async def main():
+    print("Ready on Notification")
+    await asyncio.gather(run(track[0]), run(track[1]), run(track[2]), run(track[3]), run(track[4])
+                         , run(track[5]), run(track[6]), run(track[7]), run(track[8]))
+
+# Start
 if __name__ == '__main__':
-    run(selID)
+    asyncio.run(main())
